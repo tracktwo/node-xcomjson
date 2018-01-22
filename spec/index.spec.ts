0e1f1d90c395371ea2685900b0d95d0157a0441c
tracktwo <jcemmett1@gmail.com>
2018-01-21 20:50:00 -0800

    Updates

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -2,28 +2,30 @@ import { json2xcom, xcom2json } from "../src/index";
 import * as tmp from "tmp";
 import * as fs from "fs";
 
-describe('usage', () => {
+describe('no arguments', () => {
     it('should print usage for xcom2json', (done) => {
-        xcom2json('', 'out', (str: string): void => {
-            expect(str).toContain("Usage:");
+        xcom2json('', '', (code: number, stdout: string, stderr: string): void => {
+            expect(code).not.toBe(0);
+            expect(stdout).toContain("Usage:");
             done();
         });
     });
 
     it('should print usage for json2xcom', (done) => {
-        json2xcom('', 'out', (str: string): void => {
-            expect(str).toContain("Usage:");
+        json2xcom('', '', (code: number, stdout: string, stderr: string): void => {
+            expect(code).not.toBe(0);
+            expect(stdout).toContain("Usage:");
             done();
         });
     });
 });
 
-describe('read', () => {
+describe('xcom2json', () => {
     it('should convert a file to json', (done) => {
         tmp.tmpName((err, path: string): void => {
-            xcom2json('spec/save203', path, (str: string): void => {
+            xcom2json('spec/save203', path, (code: number, stdout: string, stderr: string): void => {
+                expect(code).toBe(0);
                 fs.readFile(path, "utf-8", (err, contents: string): void => {
-                    console.log(path);
                     let json = JSON.parse(contents);
                     expect(json['header']['version']).toBe(16);
                     fs.unlink(path, () => {
@@ -37,12 +39,13 @@ describe('read', () => {
     it('should error on a bad version', (done) => {
         tmp.tmpName((err, outFile: string): void => {
             tmp.file((err, inFile, fd) => {
-                let buf : Buffer = Buffer.alloc(16);
+                let buf: Buffer = Buffer.alloc(16);
                 buf.writeUInt32LE(12, 0);
                 let nWritten = fs.writeSync(fd, buf, 0, 4);
                 expect(nWritten).toBe(4);
-                xcom2json(inFile, outFile, (s) => {
-                    expect(s).toBe('asdf');
+                xcom2json(inFile, outFile, (code: number, stdout: string, stderr: string) => {
+                    expect(code).not.toBe(0);
+                    expect(stderr).toContain('Error: unsupported version: 12');
                     done();
                 });
             });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-
 import * as cp from "child_process";
+import * as path from 'path';
 
-
-export function xcom2json(inFile: string, outFile: string, cb: (s: string) => void): void {
-   cp.execFile("dist/xcom2json", [inFile, "-o", outFile], (error, stdout, stderr) => {
-       cb(stdout);
-   });
+export function xcom2json(inFile: string, outFile: string, cb: (c: number, so: string, se: string) => void): void {
+    cp.execFile(path.resolve(__dirname, "../xcom2json"), [inFile, "-o", outFile], (error: any, stdout: string, stderr: string) => {
+        if (error != null) {
+            cb(error['code'] as number, stdout, stderr);
+        }
+        else {
+            cb(0, stdout, stderr);
+        }
+    });
 }
 
-export function json2xcom(inFile: string, outFile: string, cb: (s: string) => void): void {
-    cp.execFile("dist/json2xcom", [inFile, "-o", outFile], (error, stdout, stderr) => {
-        cb(stdout);
+export function json2xcom(inFile: string, outFile: string, cb: (c: number, so: string, se: string) => void): void {
+    cp.execFile(path.resolve(__dirname, "../json2xcom"), [inFile, "-o", outFile], (error: any, stdout: string, stderr: string) => {
+        if (error != null) {
+            cb(error['code'] as number, stdout, stderr);
+        }
+        else {
+            cb(0, stdout, stderr);
+        }
     });
 }
