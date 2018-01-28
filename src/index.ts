8ee45894f44e57c1811596ed5053635c0eb59eea
tracktwo <jcemmett1@gmail.com>
2018-01-27 21:32:26 -0800

    Moving binaries to bin

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as cp from "child_process";
 import * as path from 'path';
 
 export function xcom2json(inFile: string, outFile: string, cb: (c: number, so: string, se: string) => void): void {
-    cp.execFile(path.resolve(__dirname, "../xcom2json"), [inFile, "-o", outFile], (error: any, stdout: string, stderr: string) => {
+    cp.execFile(path.resolve(__dirname, "../../bin/xcom2json"), [inFile, "-o", outFile], (error: any, stdout: string, stderr: string) => {
         if (error != null) {
             cb(error['code'] as number, stdout, stderr);
         }
@@ -13,7 +13,7 @@ export function xcom2json(inFile: string, outFile: string, cb: (c: number, so: s
 }
 
 export function json2xcom(inFile: string, outFile: string, cb: (c: number, so: string, se: string) => void): void {
-    cp.execFile(path.resolve(__dirname, "../json2xcom"), [inFile, "-o", outFile], (error: any, stdout: string, stderr: string) => {
+    cp.execFile(path.resolve(__dirname, "../../bin/json2xcom"), [inFile, "-o", outFile], (error: any, stdout: string, stderr: string) => {
         if (error != null) {
             cb(error['code'] as number, stdout, stderr);
         }
